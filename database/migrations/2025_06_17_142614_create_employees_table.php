e3ff8e916effbe6466f232637b8e88d93fefb4c7
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-18 00:10:41 +0600

    Done

--- database/migrations/2025_06_17_142614_create_employees_table.php
+++ database/migrations/2025_06_17_142614_create_employees_table.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Primary Key
+            $table->string('id_card_number')->unique();
+            $table->string('name');
+            $table->string('position');
+            $table->string('department');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->text('address');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('employees');
