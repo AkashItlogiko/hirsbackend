b99a43ec704be76f6a49ac3aae9557ba22f563ac
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-17 20:26:49 +0600

    Done

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,6 @@ Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
         Route::post('logout', 'AuthenticationController@logOut')->name('logout');
+        Route::get('employees', [EmployeeController::class,'index']);
     });
 });
